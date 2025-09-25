c96582a6c51c7cef585892e28e9080ee67594aec
Anil Pal <anilcodes01@gmail.com>
2025-09-25 14:18:47 +0530

    fixed vercel errors

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,17 +1,15 @@
 import Sidebar from "../components/main/Sidebar";
-import { getPendingInvitationCount } from "../actions/InvitationActions";
 
 export default  async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-    const invitationCount = await getPendingInvitationCount();
 
   return (
     // This div takes the full height of its parent (the padded <main> tag).
     <div className="flex bg-zinc-900 h-full">
-        <Sidebar initialInvitationCount={invitationCount} />
+        <Sidebar  />
       {/* This main area will fill the remaining space and handle its own scrolling. */}
       <main className="flex-1 overflow-y-auto">
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 
 import Sidebar from './components/main/Sidebar';
 import MainContent from './components/main/MainContent';
-import { getPendingInvitationCount } from './actions/InvitationActions';
 
 export default async function DashboardPage() {
 
-    const invitationCount = await getPendingInvitationCount();
   return (
     <div className="flex h-full bg-zinc-900">
-      <Sidebar initialInvitationCount={invitationCount} />
+      <Sidebar />
       <main className="flex-1 bg-zinc-900 mt-16 overflow-y-auto">
         <MainContent />
       </main>
